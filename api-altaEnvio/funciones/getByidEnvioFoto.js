7ba5360cf27154d92e12b5413a0d86ce8f1d92cb
Agustt123 <agustintracheskyoficial@gmail.com>
2025-08-01 16:45:35 -0300

    dsdad

--- api-altaEnvio/funciones/getByidEnvioFoto.js
+++ api-altaEnvio/funciones/getByidEnvioFoto.js
@@ -3,27 +3,27 @@ const { executeQuery } = require('../dbconfig');
 async function getEnvioFotoByDid(connection, did) {
     try {
         const query = `
-            SELECT 
-                e.did,
-                e.lote,
-                e.choferAsignado,
-                e.fecha_inicio,
-                su.nombre AS nombreChofer,
-                edd.localidad,
-                edd.calle,
-                edd.numero,
-                edd.address_line,
-                ef.nombre AS nombreFoto
-            FROM envios AS e
-            LEFT JOIN envios_direcciones_destino AS edd
-                ON edd.didEnvio = e.did AND edd.elim = 0 AND edd.superado = 0
-            LEFT JOIN envios_fotos AS ef
-                ON ef.didEnvio = e.did AND ef.elim = 69
-            LEFT JOIN sistema_usuarios AS su
-                ON su.did = e.choferAsignado AND su.elim = 0 AND su.superado = 0
-            WHERE e.did = ?
-            LIMIT 1
-        `;
+    SELECT 
+        e.did,
+        e.lote,
+        e.choferAsignado,
+        DATE_SUB(e.fecha_inicio, INTERVAL 3 HOUR) AS fecha_inicio,
+        su.nombre AS nombreChofer,
+        edd.localidad,
+        edd.calle,
+        edd.numero,
+        edd.address_line,
+        ef.nombre AS nombreFoto
+    FROM envios AS e
+    LEFT JOIN envios_direcciones_destino AS edd
+        ON edd.didEnvio = e.did AND edd.elim = 0 AND edd.superado = 0
+    LEFT JOIN envios_fotos AS ef
+        ON ef.didEnvio = e.did AND ef.elim = 69
+    LEFT JOIN sistema_usuarios AS su
+        ON su.did = e.choferAsignado AND su.elim = 0 AND su.superado = 0
+    WHERE e.did = ?
+    LIMIT 1
+`;
         const [result] = await executeQuery(connection, query, [did]);
         return { estado: true, data: result } || { estado: false, data: null };
     } catch (error) {
