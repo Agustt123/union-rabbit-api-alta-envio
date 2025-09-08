da1868bb15b3b4c379ae20e49ae04393d2eac58d
Agustt123 <agustintracheskyoficial@gmail.com>
2025-09-08 15:45:14 -0300

    DASDA

--- rabbit-altaFF/controllerAlta/controllerAltaEnvio.js
+++ rabbit-altaFF/controllerAlta/controllerAltaEnvio.js
@@ -102,11 +102,12 @@ async function checkExistingShipment(data, connection) {
   const queryCheck = `
     SELECT ml_vendedor_id, ml_shipment_id 
     FROM envios 
-    WHERE ml_shipment_id = ? AND elim IN (0,52) AND superado = 0
+    WHERE ml_shipment_id = ? AND elim IN (0,52) AND superado = 0 AND didCliente = ?
   `;
 
   const result = await executeQuery(connection, queryCheck, [
-    data.data.ml_shipment_id
+    data.data.ml_shipment_id,
+    data.data.didCliente
   ]);
 
   console.log("🚨 Cantidad de coincidencias:", result.length);
