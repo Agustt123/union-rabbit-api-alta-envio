e7c8dfc0c181f89f3203b20e61f41c65b5122f0d
Agustt123 <agustintracheskyoficial@gmail.com>
2025-12-15 11:40:36 -0300

    dsadadsad

--- api-altaEnvio/fuctions/sendToshipmentStateMicroserviceApi.js
+++ api-altaEnvio/fuctions/sendToshipmentStateMicroserviceApi.js
@@ -29,7 +29,7 @@ function generarTokenFechaHoy() {
 }
 
 
-/*async function sendToShipmentStateMicroServiceAPI(companyId, userId, shipmentId, estado) {
+async function sendToShipmentStateMicroServiceAPI(companyId, userId, shipmentId, estado) {
 
     const message = {
         didempresa: companyId,
@@ -61,62 +61,6 @@ function generarTokenFechaHoy() {
 
         throw error;
     }
-}*/
-async function sendToShipmentStateMicroServiceAPI(
-    companyId,
-    userId,
-    shipmentId,
-    estado,
-    latitud = null,
-    longitud = null,
-    db
-) {
-    const message = {
-        didempresa: companyId,
-        didenvio: shipmentId,
-        estado: estado,
-        subestado: null,
-        estadoML: null,
-        fecha: getFechaUTC3(),
-        quien: userId,
-        operacion: 'Altamasiva',
-        latitud,
-        longitud,
-        desde: "Altamasiva",
-        tkn: generarTokenFechaHoy(),
-    };
-
-    try {
-        if (true) {
-            await actualizarEstadoLocal(db, [shipmentId], "Altamasiva", message.fecha, userId, message.estado);
-            return;
-        }
-        const response = await axiosInstance.post(urlMicroserviciosEstado, message);
-    } catch (httpError) {
-        console.error('Error enviando a Shipment State MicroService API:', httpError.message);
-    }
 }
 
-async function actualizarEstadoLocal(db, shipmentIds, deviceFrom, dateConHora, userId, state) {
-    const query1 = `
-            UPDATE envios_historial
-            SET superado = 1
-            WHERE superado = 0 AND didEnvio IN(${shipmentIds.join(',')})
-        `;
-    await executeQuery(db, query1);
-
-    const query2 = `
-            UPDATE envios
-            SET estado_envio = ?
-            WHERE superado = 0 AND did IN(${shipmentIds.join(',')})
-        `;
-    await executeQuery(db, query2, [state]);
-
-    const query3 = `
-            INSERT INTO envios_historial (didEnvio, estado, quien, fecha, didCadete, desde)
-            SELECT did, ?, ?, ?, choferAsignado, ?
-            FROM envios WHERE did IN(${shipmentIds.join(',')})
-        `;
-    await executeQuery(db, query3, [state, userId, dateConHora, deviceFrom]);
-}
-module.exports = { sendToShipmentStateMicroServiceAPI, getFechaUTC3, generarTokenFechaHoy };
+module.exports = sendToShipmentStateMicroServiceAPI;
