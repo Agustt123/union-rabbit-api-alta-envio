8f971be778e47e88e26372d244b17882282a7a34
Agustt123 <agustintracheskyoficial@gmail.com>
2025-12-17 11:56:58 -0300

    dsadsada

--- api-altaEnvio/controllerAlta/controllerAltaEnvioMisPicho.js
+++ api-altaEnvio/controllerAlta/controllerAltaEnvioMisPicho.js
@@ -20,7 +20,7 @@ async function AltaEnvioMisPicho(company, connection, data) {
             delivery_preference: data.data.delivery_preference || "",
             ml_shipment_id: data.data.ml_shipment_id || "",
             ml_qr_seguridad: data.data.ml_qr_seguridad || "",
-            ml_venta_id: data.data.idenvio || "",
+            ml_venta_id: data.data.id_venta || "",
             ml_vendedor_id: data.data.id_seller || "",
             didCliente: data.data.didCliente,
             didCuenta: data.data.didCuenta,
